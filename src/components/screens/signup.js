ee23f393cabf49a1a83b23414bee83441a645329
William Irsal <l0calh34rtz@gmail.com>
2020-06-02 22:28:50 +0700

    server integration to UI

--- src/components/screens/signup.js
+++ src/components/screens/signup.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import {Link } from 'react-router-dom'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
 
 const Signup = ()=>{
+    const [name,setName] = useState("")
+    const [password,setPassword] = useState("")
+    const [email,setEmail] = useState("")
+    const PostSignup =()=>{
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name:"",
+                password:"",
+                email:""
+            })
+        }).then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+            })
+        }
+
     return (
         <div className="mycard">
             <div className="card auth-card input-field">
@@ -9,16 +29,23 @@ const Signup = ()=>{
                 <input
                     type="text"
                     placeholder="name"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #90a4ae blue-grey darken-1">
+                <button className="btn waves-effect waves-light #90a4ae blue-grey darken-1"
+                onClick={()=>PostSignup()}>
                     Register
                 </button>
                 <h5>
